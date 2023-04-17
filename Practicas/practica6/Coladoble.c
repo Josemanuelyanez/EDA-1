53610c6b43eed290a50ecdb876257c50383faa34
José Manuel Yañez <josemanuelyanez1203@gmail.com>
2023-04-17 21:22:19 +0000

    Practica 6 corregida

--- Practicas/practica6/Coladoble.c
+++ Practicas/practica6/Coladoble.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
-bool isPalindrome(char str[]) {
-    int len = strlen(str);
-    char deque[len];
+#define MAX 100
+
+int main() {
+    char input[MAX];
+    char deque[MAX];
     int front = -1, rear = -1;
-    
-    for (int i = 0; i < len; i++) {
-        char c = str[i];
-        if (isalnum(c)) {  
+
+    printf("Ingrese una cadena de caracteres: ");
+    fgets(input, MAX, stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    int i = 0;
+    while (input[i] != '\0' && i < MAX) {
+        char c = input[i];
+        if (c >= 'A' && c <= 'Z') {
+            c = tolower(c);
+        }
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
             if (front == -1) {
                 front = 0;
             }
             rear++;
-            deque[rear] = tolower(c);
+            deque[rear] = c;
         }
+        i++;
     }
-    
+
     while (front < rear) {
         if (deque[front] != deque[rear]) {
-            return false;
+            printf("La cadena no es un palíndromo.\n");
+            return 0;
         }
         front++;
         rear--;
     }
-    return true;
-}
-
-int main() {
-    char input[100];
-    printf("Ingrese una cadena de caracteres: ");
-    fgets(input, 100, stdin);
-    input[strcspn(input, "\n")] = 0;
-    if (isPalindrome(input)) {
-        printf("La cadena es un palíndromo.\n");
-    } else {
-        printf("La cadena no es un palíndromo.\n");
-    }
+    printf("La cadena es un palíndromo.\n");
     return 0;
 }
